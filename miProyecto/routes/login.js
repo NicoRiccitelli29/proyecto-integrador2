e0a9871fce88d906b3ee5fb02161d9ff7905de68
olivia <orozze@udesa.edu.ar>
2020-09-21 13:45:45 -0300

    login

--- miProyecto/routes/login.js
+++ miProyecto/routes/login.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let loginController = require('../controllers/loginController');
+const { Router } = require('express');
 
 router.get('/', loginController.log)
 
