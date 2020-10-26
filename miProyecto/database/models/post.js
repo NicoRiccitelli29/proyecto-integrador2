4007b01a30cf8f0e36f0ca2882b3fa3433f26123
Nico <riccitelline@gmail.com>
2020-10-26 16:25:16 -0300

    TABLA DE POST

---
+++ miProyecto/database/models/post.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Post"
+
+    let cols = {
+       id: {
+            autoIncremental: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        usuarios_id : {
+            type: dataTypes.INTEGER,
+        },
+        url_imagen :{
+            type: dataTypes.VARCHAR,
+        },
+        texto: {
+            type: dataTypes.STRING,
+        },
+        fecha_de_creacion : {
+            type:dataTypes.DATETIME,
+        }
+    }
+    let config = {
+        timestamps: false,
+    }
+    const Post = sequelize.define(alias, cols, config);
+    return Post;
+}
