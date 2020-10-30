37eb46bd9d205fe243181d506fb16e626225ceff
Nico <riccitelline@gmail.com>
2020-10-30 10:54:31 -0300

    Empece a hacer login pero me da falla

--- miProyecto/controllers/loginController.js
+++ miProyecto/controllers/loginController.js
@@ -1,8 +1,17 @@
+const bcrypt = require('bcryptjs');
+const db = require('../database/models');
+const usuario = db.User;
+
+const op = db.Sequelize.Op;
+
+
+
 let loginController ={
     log: function(req, res) {
         return res.render('login' );
     }
 }
+
 
 
 
