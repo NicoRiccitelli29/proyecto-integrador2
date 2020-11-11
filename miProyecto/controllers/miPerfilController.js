3d07658fbee02153bb998dccf2ecd781c48c78aa
olivia <orozze@udesa.edu.ar>
2020-11-11 00:15:06 -0300

    empezando mi perfil controller

--- miProyecto/controllers/miPerfilController.js
+++ miProyecto/controllers/miPerfilController.js
@@ -1,9 +1,34 @@
 let miPerfilController ={
-    perfil : function(req, res) {
-        return res.render('miPerfil');
-    }
+
+    perfil: function(req, res, next){
+        let idUsuarioAMostrar = req.params.id
+        db.usuarios.findOne(
+            {
+                where: [
+                    { id: idUsuarioAMostrar },
+                    
+                ],
+                include: [ 
+                    {
+                        association: "seguidoress"
+                    },
+                    {
+                        association: "posteos"
+                    },
+                    {
+                        association: "seguido"  
+                    }
+                ]
+            }
+        )
+        .then(function(usuario) {
+           res.render("miPerfil" ,{usuario: usuario});
+        })
+        
+    },
     
 }
+
 
 
 module.exports = miPerfilController
