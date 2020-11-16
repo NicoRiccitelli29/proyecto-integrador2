e92ca17d4307451fbdcea5307663ed82584b8fe6
Demian <dlazarte@udesa.edu.ar>
2020-11-16 14:33:27 -0300

    detallePost y detalleUsuario

--- miProyecto/routes/detallePost.js
+++ miProyecto/routes/detallePost.js
@@ -3,5 +3,7 @@ let router = express.Router();
 let detallePostController = require('../controllers/detallePostController');
 
 router.get("/", detallePostController.index);
+router.get('/:id', detallePostController.porId);
+router.get('/:id/comentar', detallePostController.comentario);
 
 module.exports = router;
