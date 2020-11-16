4c66ff156bd4e548fb4edb4174029f9b0a3bd42d
Demian <dlazarte@udesa.edu.ar>
2020-11-16 13:10:05 -0300

    miPerfil arreglado

--- miProyecto/routes/miPerfil.js
+++ miProyecto/routes/miPerfil.js
@@ -1,14 +1,9 @@
 let express = require('express');
 let router = express.Router();
 let miPerfilController = require('../controllers/miPerfilController');
-const { Router } = require('express');
 
-<<<<<<< Updated upstream
-router.get('/:id', miPerfilController.perfil);
-=======
-//router.get('/:id', miPerfilController.perfil)
+
 router.get('/', miPerfilController.perfil);
->>>>>>> Stashed changes
 
 
 
